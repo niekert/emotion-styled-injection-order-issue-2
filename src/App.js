df75614dc2dce47e0d84f35c108d81f73ffa1b27
Niek Kruse <niekkruse70@gmail.com>
2018-05-16 12:44:23 +0200

    Try to import styled components first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { Card } from './style';
+// Import styled first
+import styled from 'styled-components';
 import OverridenComponent from './OverridenComponent';
+import { Card } from './style';
+
+const StyledComponent = styled.div`
+  background: red;
+`;
 
 class App extends Component {
   render() {
     return (
       <div>
+        <StyledComponent>Red</StyledComponent>
         <Card>This card is not overriden, it has a purple background.</Card>
         <OverridenComponent />
       </div>
